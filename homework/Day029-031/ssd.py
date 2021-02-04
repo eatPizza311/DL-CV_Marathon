e30a469642d6c56c46a86c4ca952d8b01047ac86
ChihYing <rockids@hotmail.com.tw>
2021-02-04 11:33:15 +0800

    feat: fix demo.ipynb bugs with depricated autograd in pytorch

--- homework/Day029-031/ssd.py
+++ homework/Day029-031/ssd.py
@@ -31,7 +31,7 @@ class SSD(nn.Module):
         self.num_classes = num_classes
         self.cfg = (coco, voc)[num_classes == 21]
         self.priorbox = PriorBox(self.cfg)
-        self.priors = Variable(self.priorbox.forward(), volatile=True)
+        self.priors = Variable(self.priorbox.forward())
         self.size = size
 
         # SSD network
@@ -46,7 +46,7 @@ class SSD(nn.Module):
         if phase == 'test':
             self.softmax = nn.Softmax(dim=-1)
             self.detect = Detect(num_classes, 0, 200, 0.01, 0.45)
-
+            
     def forward(self, x):
         """Applies network layers and ops on input image(s) x.
 
@@ -96,12 +96,13 @@ class SSD(nn.Module):
         loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
         conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
         if self.phase == "test":
-            output = self.detect(
+            output = self.detect.forward(
                 loc.view(loc.size(0), -1, 4),                   # loc preds
                 self.softmax(conf.view(conf.size(0), -1,
                              self.num_classes)),                # conf preds
                 self.priors.type(type(x.data))                  # default boxes
             )
+
         else:
             output = (
                 loc.view(loc.size(0), -1, 4),
